b21d82c8e56720941a3ba1b28b67b0e2149dbbc0
shrutibuwa27 <112264023+shrutibuwa27@users.noreply.github.com>
2024-04-23 23:55:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 import socket
-import os
-soc = socket.socket()
-host = "127.0.0.1"
-port = 8084
-soc.bind((host,port))
-soc.listen(1)
-print("Host Name:",host)
-print("Waiting For Client To Connect")
-conn, addr =soc.accept()
-print(addr, "Client Has Connected To Server")
-fileName = input(str("File Name:"))
-file = open(fileName, 'rb')
-fileData = file.read(1024)
-conn.send(fileData)
-print("File Sent")
+def send_file(conn, filename):
+with open(filename, 'rb') as file:
+data = file.read(1024)
+while data:
+conn.send(data)
+data = file.read(1024)
+def main():
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 12345))
+server_socket.listen(5)
+print("Server is listening on port 12345...")
+while True:
+conn, addr = server_socket.accept()
+print(f"Client connected from {addr}")
+filename = conn.recv(1024).decode()
+print(f"Receiving file: {filename}")
+send_file(conn, filename)
+print(f"File {filename} sent successfully!")
+conn.close()
+if __name__ == '__main__':
+main()
